6f16715bfa8f3a1beb7ad2a30842a3adf5638b08
enzo <1mm0rt4le@gmail.com>
2018-06-15 15:01:52 +0200

    Added tabs, sibilingsNum, ecc.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ graph = {
         Node dictionary
         id=>nodeObject
      */
-    nodes: {},
+    nodes: new Map(),
     //Given an id, returns the node object
     getNode: function (id) {
         var node = this.nodes[id];
@@ -20,26 +20,34 @@ graph = {
     createNode(nodeData) {
         var id = nodeData.id;
 
+
         //If node already there, just return it
-        if (this.nodes[id])
-            return this.nodes[id];
+        if (this.nodes.has(id))
+            return this.nodes.get(id);
 
         //Node object
         var node = {
             id: id,
             originalData: nodeData,
+            //position on the vertical axis
             depth: -1000,
+            //how many siblings has the node
+            siblingsNum: 0,
+            // The order between siblings
+            order: 0,
+            //Nesting level
+            tab: 0,
             //Dictionary of nodes connected to this node "on top" part of it
-            parents: {},
+            parents: new Map(),
             //Dictionary of nodes connected to this node "on bottom" part of it.
-            childs: {},
+            childs: new Map(),
             //Add a parent to the dictionary, indexed by id
             addParent: function (parentNode) {
-                this.parents[parentNode.id] = parentNode;
+                this.parents.set(parentNode.id, parentNode);
             },
             //Add a child to the dictionary, indexed by id
             addChild: function (childNode) {
-                this.childs[childNode.id] = childNode;
+                this.childs.set(childNode.id, childNode);
             },
             //Updates the depth of the current node and all his children
             setDepth: function (newDepth) {
@@ -49,36 +57,78 @@ graph = {
 
                 this.depth = newDepth;
                 //For each child set its depth
-                for (var key in this.childs) {
-                    this.childs[key].setDepth(newDepth + 1);
+                this.childs.forEach(child => child.setDepth(newDepth + 1));
+            },
+            setTab: function (newTab) {
+
+                this.tab = newTab;
+                var childDepths = new Map();
+                this.childs.forEach(child => childDepths.set(child.depth, 1 + (childDepths.get(child.depth) || 0)));
+
+                var toDo = null;
+                console.log(childDepths);
+                if (childDepths.size === 0) {
+                    //No childs
+                    console.log("nochilds");
+                    return;
                 }
+                else if (childDepths.size === 1) {
+                    //Childs are all on the same depth
+                    toDo = (child) => child.setTab(newTab);
+                    console.log("+0")
+                }
+                else if (childDepths.size === 2) {
+                    //The childs of this have 2 different levels of depth so it is the case to use tabs
+                    var maxDepth = 0;
+
+                    for (var depth of childDepths.keys())
+                        maxDepth = depth > maxDepth ? depth : maxDepth;
+
+                    toDo = (child) => {
+                        //The child which is at maximum depth is the merge node so it should not be tabbed
+                        child.depth === maxDepth ? child.setTab(newTab) : child.setTab(newTab + 1);
+                    }
+                }
+                else {
+                    //Complex network
+                    throw "not yet implemented";
+                }
+
+                this.childs.forEach(child => toDo(child));
+            },
+            updateChildsHorizontalPosition: function () {
+                var order = 0;
+                this.childs.forEach(child => {
+                    child.siblingsNum = this.childs.size;
+                    child.order = order;
+                    order++;
+                    child.updateChildsHorizontalPosition();
+                });
             }
         };
 
         //add the created node to the list of nodes of this graph
-        this.nodes[id] = node;
+        this.nodes.set(id, node);
 
         //end of createNode()
         return node;
     },
     addParentChildRel: function (parentId, childId) {
-
-        var parentNode = this.getNode(parentId);
-        var childNode = this.getNode(childId);
         //if parent or child are null throw error
-        if (!parentNode || !childNode)
+        if (!this.nodes.has(childId) || !this.nodes.has(parentId))
             throw "error parent or child not found";
+
+        var parentNode = this.nodes.get(parentId);
+        var childNode = this.nodes.get(childId);
 
         parentNode.addChild(childNode);
         childNode.addParent(parentNode);
     },
     //Returns the only node without any parents
     getRoot() {
-        for (var key in this.nodes) {
-            //if the lenght of the parents array of the current node is 0 it is the root
-            if (Object.keys(this.nodes[key].parents).length === 0) {
-                return this.nodes[key];
-            }
+        for (var node of this.nodes.values()) {
+            if (node.parents.size === 0)
+                return node;
         }
 
         throw "No root found";
@@ -87,6 +137,14 @@ graph = {
     updateDepth() {
         var root = this.getRoot();
         root.setDepth(0);
+    },
+
+    updateTabs() {
+        this.getRoot().setTab(0);
+    },
+
+    updateChildsPosition() {
+        this.getRoot().updateChildsHorizontalPosition();
     }
 
 };
@@ -106,6 +164,8 @@ function createGraph(neuralNetwork) {
     });
 
     graph.updateDepth();
+    graph.updateTabs();
+    graph.updateChildsPosition();
 
     console.log("graph");
     console.log(graph);
