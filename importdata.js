52ccc931c408af68f31b2d229c910f8f80fa330b
alexstat123 <alexander.statsenko198@gmail.com>
2018-06-20 22:35:33 +0200

    drawing of neural network

--- importdata.js
+++ importdata.js
@@ -1,87 +1,138 @@
 
 
-function importData()
-{
-	//alert("javascript working very well");
-	d3.json("AnnaNet/net.json",function(data)
-	{
-		console.log("AnnaNet/net.json");
-		console.log(data);
+function getdata(graph){
 
-		// diplay number of layer in the console
-		console.log(data.netLayers.length);
-	})
+    console.log("my script is working!");
+    console.log("grap data is:",graph);
 
-	d3.json("AnnaNet/training.json",function(data)
-	{
-		console.log("training.json");
-		console.log(data);
-	})
+    var name = [];
+    var depthArr = [];
+    var siblingArr = [];
+    var order = [];
+    graph["nodes"].forEach(function (item,index,array) {
 
-	//Layers JSON files 
+        depthArr.push(item["depth"]);
+        siblingArr.push(item["siblingsNum"]);
+        name.push(item["id"]);
+        order.push(item["order"]);
 
-	d3.json("AnnaNet/layers/'bn1.json",function(data)
-	{
-		console.log("'bn1.json");
-		console.log(data);
-	})
+    })
+    console.log(name);
+    console.log(depthArr);
+    console.log(siblingArr);
+    console.log(order);
+
+    drawRectangle(depthArr,name,siblingArr)
+
 }
 
 
 
 
-
-
-
-
-
-
-function drawRectangle2(number_of_layers)
+function drawRectangle(depthArr,name,siblingsArr)
 {
-	var gap = 1;
-	var barthinkness = 20;
-	var barwidth = 200;
-	var xpos = 100;
-
-	var svg = d3.select("body")
-	.append("svg")
-	.attr("width", 400)
-	.attr("height", 1800);
-
-	d3.json("AnnaNet/net.json",function(data)
-	{
-		
-		var rects = svg.selectAll("foo")
-					.data(data.netLayers)
-					.enter()
-					.append("rect")
-					.attr("x", function(d,i)
-						{
-
-							return xpos
-						})
-					.attr("y", function(d,i){return i*(barthinkness+gap)})
-					.attr("width", barwidth)
-					.attr("height", barthinkness)
-					.attr("fill", "teal");
 
 
-	     var label = svg.selectAll("text") 
-	                .data(data.netLayers)
-	                .enter()
-	                .append("text")        
-    				.style("fill", "black")   
-    				.attr("x", 200)           
-    				.attr("y", function(d,i){return i*(barthinkness+gap)})           
-    				.attr("dy", ".75em")           
-    				.attr("text-anchor", "middle") 
-    				.text(function(data,i){return data.id});
+    var gap = 1;
+    var barthinkness = 20;
+    var barwidth = 200;
+    var xpos = 0;
+
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", 1000)
+        .attr("height", 1800);
 
 
 
-	})
+        var rects = svg.selectAll("foo")
+            .data(depthArr)
+            .enter()
+            .append("rect")
+            .attr("x", function(d,i)
+            {
+
+                var indexes = getAllIndexes(depthArr, d);
+
+                if(indexes.length > 1){
+
+                    console.log("my length is", indexes.length);
+                    console.log("out of my siblings i am: ",indexes.indexOf(i));
+                    return xpos + 201*indexes.indexOf(i);
+                }
 
 
 
-	
+            })
+            .attr("y", function(d,i){
+
+                return d*(barthinkness+gap)})
+            .attr("width", barwidth)
+            .attr("height", barthinkness)
+            .attr("fill", "teal");
+
+
+        var label = svg.selectAll("text")
+            .data(depthArr)
+            .enter()
+            .append("text")
+            .style("fill", "black")
+            .attr("x", function (d,i) {
+
+                var indexes = getAllIndexes(depthArr, d);
+                //console.log("find all indexes of ",indexes);
+                if(indexes.length > 1){
+
+                    console.log("my length is", indexes.length);
+                    console.log("out of my siblings i am: ",indexes.indexOf(i));
+                    return 100 + 200*indexes.indexOf(i);
+                }else{
+                    return 100;
+                }
+            })
+            .attr("y", function(d,i){
+
+                return d*(barthinkness+gap)})
+            .attr("dy", ".75em")
+            .attr("text-anchor", "middle")
+            .text(function(d,i){
+
+                //console.log("name",name[i]);
+                return name[i];
+            });
+
 }
+
+depthCheckArr = [];
+function siblings(data){
+
+
+    depthCheckArr.push(data);
+
+
+    return countInArray(depthCheckArr, data)
+}
+
+
+function countInArray(array, what) {
+    var count = -1;
+    var newcount=0;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === what) {
+            count++;
+        }
+    }
+
+
+    return count;
+}
+
+
+function getAllIndexes(arr, val) {
+    var indexes = [], i = -1;
+    while ((i = arr.indexOf(val, i+1)) != -1){
+        indexes.push(i);
+    }
+    return indexes;
+}
+
