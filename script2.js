7323ce7e8f78e57407bd400102cece69ab051824
alexstat123 <alexander.statsenko198@gmail.com>
2018-07-11 00:25:59 +0200

    my latest changes

--- script2.js
+++ script2.js
@@ -88,7 +88,8 @@ function drawRectangle(depthArr, name, siblingsArr) {
             //layerOrder(graph.nodesArray[i]["depth"]);
             //console.log("check length",layerOrder(graph.nodesArray[i]["depth"]));
             //console.log("index length",indexes.length);
-
+            myDebth(graph.nodesArray,d,i);
+            width(graph.nodesArray,d,i);
 
             scalling(barwidth  / indexes.length, d.siblingsNum, indexes, i);
             return (barwidth  / indexes.length);
@@ -171,9 +172,95 @@ function scalling(width,siblingNum,depthIndexes,i){
     //console.log("wSn",wSn);
     var sum = wSn.reduce((x,y) => x + y);
     //console.log("sum",sum);
-    console.log("width",setWidth);
-    console.log("sibling num",siblingNum);
+    //console.log("width",setWidth);
+    //console.log("sibling num",siblingNum);
     var r = width/sum;
-    console.log("r",r);
+    //console.log("r",r);
+
+}
+
+widthArr = [];
+widthStorageArr = [200];
+function myDebth(graphnodesArray,node,i){
+
+    // identify parent of this node
+    gA = graphnodesArray;
+    me =node.id;
+    p = node.parents;
+
+
+    // extract parent name from Map
+    var myMap = p
+    for (var [key] of myMap) {
+
+    }
+
+
+    // find index of parent
+    var data = gA;
+    var index = -1;
+    var val = key;
+    var filteredObj = data.find(function(item, i){
+        if(item.id === val){
+            index = i;
+            return i;
+        }
+    });
+
+
+    //     //console.log("children of my parent",siblingArr[index]);
+    //     console.log("i am ",me);
+    //     console.log("my parent is ",key);
+    //     console.log("my parent index is",index);
+    //     console.log("children of my parent",siblingArr[index]);
+
+
+    //console.log("i am ",me);
+    //console.log("my parent is ",key);
+    //console.log("my parent index is",index);
+    if(index > -1){
+        numChildrenParent = graph.nodesArray[index]["childs"].size;
+        //console.log("children of my parent",numChildrenParent);
+        widthStorageArr.push(widthStorageArr[index] /numChildrenParent );
+
+        // width of each node
+        //console.log(widthStorageArr);
+
+        numParent = graph.nodesArray[i]["parents"].size;
+        //console.log("parents num",numParent);
+        //console.log("children of my parent",numChildrenParent);
+
+
+        if(numParent > 1 && numChildrenParent == 1){
+            //console.log("UNITY UNITY UNITY UNITY UNITY!");
+            widthStorageArr.push(200);
+        }
+
+    }
+
+
+
+}
+
+function width(graphnodesArray,node,i){
+
+if(node.parents.size === 0){
+    node.width = 200;
+}else{
+
+    widthsum = 0;
+//sum all parents witdths
+    node.parents.forEach((key,value) => {widthsum += key.width;
+    });
+
+    node.width = widthsum / node.siblingsNum;
+    
+
+
+}
+
+console.log(node.width);
+
+
 
 }
